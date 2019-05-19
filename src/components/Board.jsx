fa82879c3876430063dd1d12f083d17482f1c558
dfreire <dario.freire@gmail.com>
2019-05-19 17:14:26 +0100

    step-1

---
+++ src/components/Board.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+// step-1.4
+import Square from "./Square";
+
+class Board extends React.Component {
+  renderSquare(i) {
+    // step-1.3
+    return <Square />;
+  }
+
+  render() {
+    // step-1.2
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Square extends React.Component {
+  render() {
+    // step-1.1
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
+
+export default Square;
