0b441244eee6e2f3daaa4fce69f88d3dda1d0376
dfreire <dario.freire@gmail.com>
2019-05-19 17:41:11 +0100

    step-3

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 
 class Square extends React.Component {
+  constructor(props) {
+    // step-3.2
+    super(props);
+    this.state = {
+      symbol: null // "X" or "O"
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          // step-3.1
+          console.log("clicked", this.props.symbol);
+          // step-3.3
+          this.setState({ symbol: "X" });
+        }}
+      >
+        {/* step-3.4 */}
+        {this.state.symbol}
+      </button>
+    );
   }
 }
 
