5d26edd63d4c4a831659d9e967165e45dd7ccf8b
dfreire <dario.freire@gmail.com>
2019-05-19 17:42:20 +0100

    removed step-3 comments

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 class Square extends React.Component {
   constructor(props) {
-    // step-3.2
     super(props);
     this.state = {
       symbol: null // "X" or "O"
@@ -14,13 +13,9 @@ class Square extends React.Component {
       <button
         className="square"
         onClick={() => {
-          // step-3.1
-          console.log("clicked", this.props.symbol);
-          // step-3.3
           this.setState({ symbol: "X" });
         }}
       >
-        {/* step-3.4 */}
         {this.state.symbol}
       </button>
     );
